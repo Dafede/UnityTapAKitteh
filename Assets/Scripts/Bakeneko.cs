54c1069c72fb10f9100f654c3cc8288b2ab2f708
unknown <dafede333@gmail.com>
2015-03-17 21:58:54 +0100

    asdasd

---
+++ Assets/Scripts/Bakeneko.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bakeneko : MonoBehaviour {
+
+	private ulong score = 0;
+	private uint actualScoreIncrement = 1;
+
+	// Use this for initialization
+	void Start () {
+		NotificationCenter.DefaultCenter ().AddObserver (this,"IncreaseScore");
+		//Score : update with saved info
+	}
+
+	void IncreaseScore(Notification notification){
+		score += actualScoreIncrement;
+		Debug.Log ("Score: "+ score);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/CatHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatHandler : MonoBehaviour {
+
+	public Sprite normalCat;
+	public Sprite touchedCat;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnMouseDown() {
+		gameObject.GetComponent<SpriteRenderer> ().sprite = touchedCat;
+		//NotificationCenter.DefaultCenter().PostNotification(this, "IncreaseScore", pointsToIncrement);
+		NotificationCenter.DefaultCenter().PostNotification(this, "IncreaseScore");
+	}
+
+	void OnMouseUp() {
+		gameObject.GetComponent<SpriteRenderer> ().sprite = normalCat;
+	}
+}
