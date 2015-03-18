e3df49597acd0eeadb98c11648010b6587b9332a
Ryuzaki <ojizaki@gmail.com>
2015-03-18 19:30:01 +0100

    pepeluis

---
+++ Assets/Scripts/MainCanvasActions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCanvasActions : MonoBehaviour {
+
+    public void OnClickShopButton() {
+        
+    }
+
+    public void OnClickInfoButton() { 
+
+    }
+}
