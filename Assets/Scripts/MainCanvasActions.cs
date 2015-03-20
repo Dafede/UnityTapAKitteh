e0e7847b9cbdce3ab6299ae3ba1360bc33d2c6ce
Ryuzaki <ojizaki@gmail.com>
2015-03-20 02:44:53 +0100

    mas mierdas de la camara

--- Assets/Scripts/MainCanvasActions.cs
+++ Assets/Scripts/MainCanvasActions.cs
@@ -3,7 +3,17 @@ using System.Collections;
 
 public class MainCanvasActions : MonoBehaviour {
 
+    public Camera mainCamera;
+    private Animator animator;
+
+
+    void Awake() {
+        
+        animator = mainCamera.GetComponent<Animator>();
+    }
+
     public void OnClickShopButton() {
+        animator.SetBool("MoveCameraLeft",true);
         
     }
 
