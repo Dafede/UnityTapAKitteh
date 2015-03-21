e213d1b572c3cdf15a4c33752bef4b1e8f4b5dc5
Ryuzaki <ojizaki@gmail.com>
2015-03-21 22:37:20 +0100

    mas y mas y mas

--- Assets/Scripts/Bakeneko.cs
+++ Assets/Scripts/Bakeneko.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class Bakeneko : MonoBehaviour {
 
@@ -8,9 +7,20 @@ public class Bakeneko : MonoBehaviour {
 	private ulong score = 0;
 	private uint actualScoreIncrement = 1;
 
+
+    public int numCincoBolsitas = 0;
+    public int numCincuentaBolsitas = 0;
+    public int numCienBolsitas = 0;
+    public int numDosCienBolsitas = 0;
+    public int numDosCienCincuentaBolsitas = 0;
+
+
+
 	// Use this for initialization
 	void Start () {
 		NotificationCenter.DefaultCenter ().AddObserver (this,"IncreaseScore");
+
+        InvokeRepeating("UpdateScore", 1.0f, 1.0f);
 		//Score : update with saved info
 	}
 
@@ -22,6 +32,12 @@ public class Bakeneko : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
+
+    private void UpdateScore() { 
+        int s = numCincoBolsitas * 5 + numCienBolsitas * 100 + numCincuentaBolsitas * 50 + numDosCienBolsitas * 200 + numDosCienCincuentaBolsitas * 250;
+        score += (ulong)s;
+        scoreText.text = score.ToString();
+    }
 }
---
+++ Assets/Scripts/ShopCanvasActions.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShopCanvasActions : MonoBehaviour {
+
+    public GameObject bakeneko;
+
+    public void OnClickCincoBolsita()
+    {
+        bakeneko.GetComponent<Bakeneko>().numCincoBolsitas++;
+    }
+    public void OnClickCincuentaBolsita()
+    {
+        bakeneko.GetComponent<Bakeneko>().numCincuentaBolsitas++;
+    }
+    public void OnClickCienBolsita()
+    {
+        bakeneko.GetComponent<Bakeneko>().numCienBolsitas++;
+    }
+    public void OnClickDosCienBolsita()
+    {
+        bakeneko.GetComponent<Bakeneko>().numDosCienBolsitas++;
+    }
+    public void OnClickDosCienCincuentaBolsita()
+    {
+        bakeneko.GetComponent<Bakeneko>().numDosCienCincuentaBolsitas++;
+    }
+
+
+}
